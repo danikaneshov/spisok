d78860a5b4a8be58d925c063d7e17dfac9e82c6c
danikaneshov <danik.aneshov@gmail.com>
2025-05-12 15:45:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Раздача статических файлов из папки public
+// Раздача статических файлов
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Все запросы перенаправляем на index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Запуск сервера
 app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+    console.log(`Сервер запущен на порту ${port}`);
 });
